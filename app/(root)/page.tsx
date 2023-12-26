b616fc83b1a31f37bc4f71b53503f13c58259717
mithunsen04 <mithunsen559@gmail.com>
2023-12-27 00:52:36 +0530

    order table and pagination done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,21 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
+import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
+import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import Collection from "@/components/shared/Collection";
-import { getAllEvents } from "@/lib/actions/event.actions";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
   const events = await getAllEvents({
-    query: "",
-    category: "",
-    page: 1,
+    query: searchText,
+    category,
+    page,
     limit: 6,
   });
 
@@ -48,18 +55,18 @@ export default async function Home() {
         </h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search <br />
-          CategoryFilter
+          <Search />
+          <CategoryFilter />
         </div>
 
         <Collection
           data={events?.data}
-          emptyTitle={"Np Events Found"}
-          emptyStateSubtext={""}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
           collectionType="All_Events"
           limit={6}
-          page={1}
-          totalPages={2}
+          page={page}
+          totalPages={events?.totalPages}
         />
       </section>
     </>
