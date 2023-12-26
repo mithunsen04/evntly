33fde771a15bd596e52230f963489769b8102b97
mithunsen04 <mithunsen559@gmail.com>
2023-12-26 20:24:38 +0530

    update the event update logic

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,5 +1,37 @@
-import EventForm from "@/components/shared/EventForm";
+// import EventForm from "@/components/shared/EventForm";
 
+// import { auth } from "@clerk/nextjs";
+
+// type UpdateEventProps = {
+//   params: {
+//     id: string;
+//   };
+// };
+
+// const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
+//   const { sessionClaims } = auth();
+
+//   const userId = sessionClaims?.userId as string;
+
+//   return (
+//     <>
+//       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+//         <h3 className="wrapper h3-bold text-center sm:text-left">
+//           Update Event
+//         </h3>
+//       </section>
+
+//       <div className="wrapper my-8">
+//         <EventForm type="Update" userId={userId} />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default UpdateEvent;
+
+import EventForm from "@/components/shared/EventForm";
+import { getEventById } from "@/lib/actions/event.actions";
 import { auth } from "@clerk/nextjs";
 
 type UpdateEventProps = {
@@ -12,6 +44,7 @@ const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const { sessionClaims } = auth();
 
   const userId = sessionClaims?.userId as string;
+  const event = await getEventById(id);
 
   return (
     <>
@@ -22,7 +55,12 @@ const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
       </section>
 
       <div className="wrapper my-8">
-        <EventForm type="Update" userId={userId} />
+        <EventForm
+          type="Update"
+          event={event}
+          eventId={event._id}
+          userId={userId}
+        />
       </div>
     </>
   );
