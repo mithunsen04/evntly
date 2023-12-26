099ad24c3acdd58c0f2e0d3ad7fade95f9859e14
mithunsen04 <mithunsen559@gmail.com>
2023-12-26 20:37:32 +0530

    realated events section done

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -100,10 +100,10 @@ const EventDetails = async ({
       </section>
 
       {/* EVENTS with the same category */}
-      {/* <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-      <h2 className="h2-bold">Related Events</h2>
+      <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Related Events</h2>
 
-      <Collection 
+        <Collection
           data={relatedEvents?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
@@ -112,7 +112,7 @@ const EventDetails = async ({
           page={searchParams.page as string}
           totalPages={relatedEvents?.totalPages}
         />
-    </section> */}
+      </section>
     </>
   );
 };
