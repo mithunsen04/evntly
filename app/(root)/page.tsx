e15c713a33d9f482be4ef54a2360fa08441f5a04
mithunsen04 <mithunsen559@gmail.com>
2023-12-26 20:02:56 +0530

    Card section done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import Collection from "@/components/shared/Collection";
+import { getAllEvents } from "@/lib/actions/event.actions";
 
 export default async function Home() {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6,
+  });
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -42,6 +51,16 @@ export default async function Home() {
           Search <br />
           CategoryFilter
         </div>
+
+        <Collection
+          data={events?.data}
+          emptyTitle={"Np Events Found"}
+          emptyStateSubtext={""}
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
